2135cfbe4dc26af06ef7000e493a7ba49d33160e
rodrigo <rodrigo.rgtic@gmail.com>
2024-01-05 12:29:47 -0300

    add marquee

--- src/components/techs/index.tsx
+++ src/components/techs/index.tsx
@@ -1,14 +1,17 @@
 import { Image, View } from "react-native"
+import { Marquee } from "@animatereactnative/marquee"
 
 import { TECHS } from "@/utils/techs"
 import { styles } from "./styles"
 
 export function Techs() {
   return (
-    <View style={styles.container}>
-      {TECHS.map((tech) => (
-        <Image key={tech.id} source={tech.img} style={styles.tech} />
-      ))}
-    </View>
+    <Marquee speed={0.5} spacing={10}>
+      <View style={styles.container}>
+        {TECHS.map((tech) => (
+          <Image key={tech.id} source={tech.img} style={styles.tech} />
+        ))}
+      </View>
+    </Marquee>
   )
 }
